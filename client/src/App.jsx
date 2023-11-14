29aaf6e82a035c466c8dff99d6591b65a69120ed
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-14 17:14:26 +0200

    Add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import GameList from "./components/game-list/GameList";
 import GameCreate from "./components/game-create/GameCreate";
 import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/games" element={<GameList />} />
                 <Route path="/games/create" element={<GameCreate />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </div>
     );
