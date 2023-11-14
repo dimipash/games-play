c57f5b7f72d00749ff39623a4080bc4d67174662
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-14 14:52:43 +0200

    initial structure and style

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header/Header";
+
+function App() {
+    return (
+        <div id="box">
+            <h1>Hello world</h1>
+            <Header />
+        </div>
+    );
+}
+
+export default App;
