1ea4f6d97be18013d239900efa697c98d9fee5d1
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-15 13:27:05 +0200

    Modify requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,20 +1,14 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = "http://localhost:3030/jsonstore/games";
 
 export const getAll = async () => {
-    const result = await request("GET", `${baseUrl}`);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 };
 export const create = async (gameData) => {
-    const response = await fetch(`${baseUrl}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(gameData),
-    });
-    const result = await response.json();
+    const result = await request.post(baseUrl, gameData);
+
     return result;
 };
