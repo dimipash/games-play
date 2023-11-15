94ed8600a883ae4d03013920b1ebf233bfecded6
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-15 07:50:48 +0200

    Show dynamic game list

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,52 +1,25 @@
 import { useEffect, useState } from "react";
 import * as gameService from "../../services/gameService";
+import GameListItem from "./game-list-item/GameListItem";
 
 export default function GameList() {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState([]);
 
     useEffect(() => {
-        gameService.getAll()    
-            .then(result => setGames(result));        
+        gameService.getAll().then((result) => setGames(result));
     }, []);
-
-    console.log(games);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
+            {games.map((game) => (
+                <GameListItem {...game} key={game._id} />
+            ))}
 
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
         </section>
     );
 }
