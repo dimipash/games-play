3f9aa816ecbfaf8bcc33e61638d58874892f92ee
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-15 14:06:32 +0200

    Add game details page

--- client/src/components/game-list/game-list-item/GameListItem.jsx
+++ client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -1,10 +1,11 @@
+import {Link} from "react-router-dom";
+
 export default function GameListItem(
     {
+        _id,
         title, 
         category,
         imageUrl,
-
-
     }
 ) {
     return (
@@ -13,9 +14,9 @@ export default function GameListItem(
                 <img src={imageUrl} />
                 <h6>{category}</h6>
                 <h2>{title}</h2>
-                <a href="#" className="details-button">
+                <Link to={`/games/${_id}`} className="details-button">
                     Details
-                </a>
+                </Link>
             </div>
         </div>
     );
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -7,6 +7,13 @@ export const getAll = async () => {
 
     return Object.values(result);
 };
+
+export const getOne = async (gameId) => {
+    const result = await request.get(`${baseUrl}/${gameId}`);
+
+    return result;
+}
+
 export const create = async (gameData) => {
     const result = await request.post(baseUrl, gameData);
 
