d6c5ab16ba0d0a3f5d99580f7b22438d16983508
Dimitar Pashev <dim.pashev@gmail.com>
2023-11-15 18:12:55 +0200

    Update comment service

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,9 +3,15 @@ import * as request from "../lib/request";
 const baseUrl = "http://localhost:3030/jsonstore/comments";
 
 export const getAll = async (gameId) => {
-    const result = await request.get(baseUrl);
-    return Object.values(result);
-}
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`,
+    });
+
+    const result = await request.get(`${baseUrl}`);
+    
+    // TODO: temp solution until migration to collections service
+    return Object.values(result).filter((comment) => comment.gameId === gameId);
+};
 
 export const create = async (gameId, username, text) => {
     const newComment = await request.post(baseUrl, {
@@ -14,4 +20,4 @@ export const create = async (gameId, username, text) => {
         text,
     });
     return newComment;
-}
+};
